0ec52b8e68ca4ae455e5af6cd2e4b6a2c31cbcc7
Madalina Mircea <mirceamariamadalina@yahoo.com>
2020-12-30 23:36:52 +0200

    initial getter and sentiment analysis

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello world")
+from SentimentAnalyser import SentimentAnalyser
+from TweetUtils import TweetUtils
+
+tu = TweetUtils()
+sa = SentimentAnalyser()
+
+result = tu.get_clean_tweets(["1311501067679485952", "1311501067801198592"])
+print(result)
+
+print(sa.get_all_tweets_scores(result))
